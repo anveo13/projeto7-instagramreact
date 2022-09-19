5204815e30c2515a0204a96efaac9255b00f50c4
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2022-09-19 13:56:16 -0300

    faltando botao curtir

--- src/Posts.js
+++ src/Posts.js
@@ -1,17 +1,31 @@
+import React from 'react';
+import './App.css';
+
+
 export default function Posts() {
+    
+    const vermelho = event => {
+        event.currentTarget.classList.toggle('bg-salmon');
+    };
+    const preto = event => {
+        event.currentTarget.classList.toggle('pretin');
+    };
+    const [contador, setContador] = React.useState(0);
+
+
     const posts = [
         {
             user: 'meowed',
             link: 'img/meowed.svg',
             following: 'respondeai',
-            likes: 'outras 101.523 pessoas',
+            likes: 'outras 101.52' ,
             content: 'img/gato-telefone.svg',
         },
         {
             user: 'barked',
             link: 'img/barked.svg',
             following: 'adorable_animals',
-            likes: 'outras 99.159 pessoas',
+            likes: 'outras 99.15',
             content: 'img/dog.svg',
         },
     ];
@@ -35,13 +49,13 @@ export default function Posts() {
 
                     <div class="fundo">
                         <div class="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
+                            <div class='container'>
+                                <ion-icon onClick={() => setContador(contador + 1)} name="heart-outline" ></ion-icon>
                                 <ion-icon name="chatbubble-outline"></ion-icon>
                                 <ion-icon name="paper-plane-outline"></ion-icon>
                             </div>
                             <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
+                                <ion-icon onClick={preto} name="bookmark-outline"></ion-icon>
                             </div>
                         </div>
 
@@ -49,7 +63,7 @@ export default function Posts() {
                             <img src="img/respondeai.svg" />
                             <div class="texto">
                                 Curtido por <strong>{post.following}</strong> e{' '}
-                                <strong>{post.likes}</strong>
+                                <strong>{post.likes}{contador} pessoas.</strong>
                             </div>
                         </div>
                     </div>
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,28 @@
+import React from "react"
 export default function Usuario() {
+
+    const fotoPadrao = "img/catanacomics.svg"
+
+    const [nome, setNome] = React.useState("catanacomics")
+    const [foto, setFoto] = React.useState(fotoPadrao)
+
+    function inserirNome(){
+        const nomePrompt = prompt("Qual é o seu usuario?")
+        setNome(nomePrompt)
+    }
+
+    function inserirFoto(){
+        const fotoPrompt = prompt("Insira o link de uma foto aqui!")
+        setFoto(fotoPrompt)
+    }
     return (
         <div class="usuario">
-            <img src="img/catanacomics.svg" />
+            <img onClick={inserirFoto} src= {foto}/>
             <div class="texto">
-                <strong>catanacomics</strong>
+                <strong>{nome}</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {nome}
+                    <ion-icon onClick={inserirNome} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
