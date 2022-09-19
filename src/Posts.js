798d36c616be5790652113ed905aed19b6d9ce93
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2022-09-19 17:43:22 -0300

    So falta contador

--- src/Posts.js
+++ src/Posts.js
@@ -3,32 +3,60 @@ import './App.css';
 
 
 export default function Posts() {
-    
-    const vermelho = event => {
-        event.currentTarget.classList.toggle('bg-salmon');
-    };
+
+
     const preto = event => {
         event.currentTarget.classList.toggle('pretin');
-    };
-    const [contador, setContador] = React.useState(0);
+    }
 
+
+    /* const [contador, setContador] = React.useState(0);
+    {() => setContador(contador + 1)}
+*/
 
     const posts = [
         {
             user: 'meowed',
             link: 'img/meowed.svg',
             following: 'respondeai',
-            likes: 'outras 101.52' ,
+            likes: 'outras 101.522',
             content: 'img/gato-telefone.svg',
         },
         {
             user: 'barked',
             link: 'img/barked.svg',
             following: 'adorable_animals',
-            likes: 'outras 99.15',
+            likes: 'outras 99.152',
             content: 'img/dog.svg',
         },
     ];
+        const [showElement, setShowElement] = React.useState(false)
+        const [nomeNovo, setVermelinho] = React.useState('heart-outline');
+    function CurtirPost() {
+        let vermelhinho = nomeNovo 
+        if (showElement) {
+            setShowElement(false)
+            vermelhinho = 'heart-outline'
+        } else {
+            setShowElement(true)
+            vermelhinho = 'heart'
+        }
+        setVermelinho(vermelhinho)
+    }
+
+    const [showElemen2t, setShowElement2] = React.useState(false)
+        const [nomeNovo2, setVermelinho2] = React.useState("bookmark-outline");
+    function CurtirPoste() {
+        let preto = nomeNovo2 
+        if (showElement) {
+            setShowElement2(false)
+            preto = 'bookmark-outline'
+        } else {
+            setShowElement2(true)
+            preto = 'bookmark'
+        }
+        setVermelinho2(preto)
+    }
     return (
         <div class="posts">
             {posts.map((post) => (
@@ -50,12 +78,12 @@ export default function Posts() {
                     <div class="fundo">
                         <div class="acoes">
                             <div class='container'>
-                                <ion-icon onClick={() => setContador(contador + 1)} name="heart-outline" ></ion-icon>
+                                <ion-icon  onClick={CurtirPost} class={nomeNovo} name={nomeNovo} ></ion-icon>
                                 <ion-icon name="chatbubble-outline"></ion-icon>
                                 <ion-icon name="paper-plane-outline"></ion-icon>
                             </div>
                             <div>
-                                <ion-icon onClick={preto} name="bookmark-outline"></ion-icon>
+                                <ion-icon onClick={CurtirPoste} class={nomeNovo2} name={nomeNovo2}></ion-icon>
                             </div>
                         </div>
 
@@ -63,7 +91,7 @@ export default function Posts() {
                             <img src="img/respondeai.svg" />
                             <div class="texto">
                                 Curtido por <strong>{post.following}</strong> e{' '}
-                                <strong>{post.likes}{contador} pessoas.</strong>
+                                <strong>{post.likes} pessoas.</strong>
                             </div>
                         </div>
                     </div>
